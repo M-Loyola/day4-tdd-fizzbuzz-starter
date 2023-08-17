08e9523b24ef4f81275bbc557a159610ed8318a7
M-Loyola <m.yakit.loyola@gmail.com>
2023-08-17 15:38:20 +0800

    feat: add handling should_return_string_FizzBuzz_when_countOff_given_a_number_multiple_of_three_and_five

--- src/main/java/com/afs/tdd/FizzBuzz.java
+++ src/main/java/com/afs/tdd/FizzBuzz.java
@@ -6,6 +6,9 @@ public class FizzBuzz {
     }
 
     public String countOff(int number) {
+        if (number %15 == 0) {
+            return "FizzBuzz";
+        }
         if (number %3 == 0) {
             return "Fizz";
         } else if (number %5 == 0) {
