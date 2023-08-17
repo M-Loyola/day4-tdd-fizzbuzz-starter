4135fe44c4e0bb5b62c90b52ccf8ef8a3b438117
M-Loyola <m.yakit.loyola@gmail.com>
2023-08-17 15:44:15 +0800

    refactor: remove redundancy in test file

--- src/test/java/com/afs/tdd/FizzBuzzTest.java
+++ src/test/java/com/afs/tdd/FizzBuzzTest.java
@@ -4,22 +4,17 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class FizzBuzzTest {
+    FizzBuzz fizzBuzz = new FizzBuzz();
     @Test
-    public void should_return_the_given_number_when_countOff_given_a_normal_number(){ //not a multiple of 3 or 5
-        //Given
+    public void should_return_the_given_number_when_countOff_given_a_normal_number(){
         int normalNumber = 1;
-        FizzBuzz fizzbuzz = new FizzBuzz();
-        //when
-        String result = fizzbuzz.countOff(normalNumber);
-
-        //Then
+        String result = fizzBuzz.countOff(normalNumber);
         Assertions.assertEquals("1", result);
     }
 
     @Test
     public void should_return_string_fizz_when_countOff_given_a_number_multiple_of_three() {
         int numberMultipleOf3 = 3;
-        FizzBuzz fizzBuzz = new FizzBuzz();
         String actualResult = fizzBuzz.countOff(numberMultipleOf3);
         Assertions.assertEquals("Fizz",actualResult);
     }
@@ -27,7 +22,6 @@ public class FizzBuzzTest {
     @Test
     public void should_return_string_buzz_when_countOff_given_a_number_multiple_of_five() {
         int numberMultipleOf5 = 5;
-        FizzBuzz fizzBuzz = new FizzBuzz();
         String actualResult = fizzBuzz.countOff(numberMultipleOf5);
         Assertions.assertEquals("Buzz",actualResult);
     }
@@ -35,7 +29,6 @@ public class FizzBuzzTest {
     @Test
     public void should_return_string_FizzBuzz_when_countOff_given_a_number_multiple_of_three_and_five() {
         int numberMultipleOf3And5 = 15;
-        FizzBuzz fizzBuzz = new FizzBuzz();
         String actualResult = fizzBuzz.countOff(numberMultipleOf3And5);
         Assertions.assertEquals("FizzBuzz", actualResult);
     }
