bec03abd850ae937de55ffe13fac32c61f429eb3
M-Loyola <m.yakit.loyola@gmail.com>
2023-08-17 15:39:57 +0800

    refactor: modify if conditioning for countOff

--- src/main/java/com/afs/tdd/FizzBuzz.java
+++ src/main/java/com/afs/tdd/FizzBuzz.java
@@ -8,8 +8,7 @@ public class FizzBuzz {
     public String countOff(int number) {
         if (number %15 == 0) {
             return "FizzBuzz";
-        }
-        if (number %3 == 0) {
+        } else if (number %3 == 0) {
             return "Fizz";
         } else if (number %5 == 0) {
             return "Buzz";
