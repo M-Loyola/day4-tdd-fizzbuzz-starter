d2b3460ac0aee7d905aec1d2ea2c1bcbc4760265
M-Loyola <m.yakit.loyola@gmail.com>
2023-08-17 16:33:18 +0800

    refactor: refactor method countOff

--- src/main/java/com/afs/tdd/FizzBuzz.java
+++ src/main/java/com/afs/tdd/FizzBuzz.java
@@ -1,26 +1,24 @@
 package com.afs.tdd;
 
 public class FizzBuzz {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
 
     public String countOff(int number) {
-        if (number %105 == 0) {
-            return "FizzBuzzWhizz";
-        } else if (number %21 == 0) {
-            return "FizzWhizz";
-        } else if (number %35 == 0) {
-            return "BuzzWhizz";
-        } else if (number %15 == 0) {
-            return "FizzBuzz";
-        } else if (number %3 == 0) {
-            return "Fizz";
-        } else if (number %5 == 0) {
-            return "Buzz";
-        } else if (number %7 == 0) {
-            return "Whizz";
-        }
+        boolean multipleOfThree = number % 3 == 0;
+        boolean multipleOfFive = number % 5 == 0;
+        boolean multipleOfSeven = number % 7 == 0;
+        boolean multipleOfThreeFive = multipleOfThree && multipleOfFive;
+        boolean multipleOfThreeSeven = multipleOfThree && multipleOfSeven;
+        boolean multipleOfFiveSeven = multipleOfFive && multipleOfSeven;
+        boolean multipleOfThreeFiveSeven = multipleOfThree && multipleOfFive && multipleOfSeven;
+
+        if (multipleOfThreeFiveSeven) {return "FizzBuzzWhizz";}
+        if (multipleOfThreeFive) {return "FizzBuzz";}
+        if (multipleOfFiveSeven) {return "BuzzWhizz";}
+        if (multipleOfThreeSeven) {return "FizzWhizz";}
+        if (multipleOfThree) {return "Fizz";}
+        if (multipleOfFive) {return "Buzz";}
+        if (multipleOfSeven) {return "Whizz";}
+
         return String.valueOf(number);
     }
 }
